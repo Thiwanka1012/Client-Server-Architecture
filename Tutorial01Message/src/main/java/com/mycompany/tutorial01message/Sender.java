d490a74c6e7f23837974c8ccead8295af5908a23
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-04-04 17:46:42 +0530

    ssss

--- Tutorial01Message/src/main/java/com/mycompany/tutorial01message/Sender.java
+++ Tutorial01Message/src/main/java/com/mycompany/tutorial01message/Sender.java
@@ -8,6 +8,10 @@ package com.mycompany.tutorial01message;
  *
  * @author Thiwanka-Gaming
  */
-public class Sender {
-    
+public class Sender{
+    public Message createMessage(String msg){
+        Message message=new Message(msg);
+        System.out.println("[Sender]-Message was Created");
+        return  message;
+    }
 }
